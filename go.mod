3b16c0860742ce00af11a9bb99873b2949cd1979
MaheshRKumawat <maheshkumawat0911@gmail.com>
2022-11-08 21:10:30 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MaheshRKumawat/Go-Raft
+
+go 1.19
---
+++ helper.go
@@ -0,0 +1,97 @@
+// All structs for Raft Consensus are here.
+// which contain Consensus Module(CM), Server, CM's state, and CM's log etc
+
+package raft
+
+import (
+	"net"
+	"net/rpc"
+	"sync"
+	"time"
+)
+
+type CMState int
+
+const DebugCM int = 1
+
+const (
+	Follower CMState = iota
+	Candidate
+	Leader
+	Dead
+)
+
+// ConsensusModule is the main struct for the Raft consensus module.
+type ConsensusModule struct {
+	id      int        // this peer's id
+	mu      sync.Mutex // Lock to protect shared access to this peer's state
+	peerIds []int      // ids of all peers (including self)
+	server  *Server    // RPC end point to receive/send RPCs from/to peers
+
+	// persistent state on all servers
+	currentTerm int        // latest term server has seen (initialized to 0 on first boot, increases monotonically)
+	votedFor    int        // candidateId that received vote in current term (or null if none)
+	log         []LogEntry // log entries; each entry contains command for state machine, and term when entry was received by leader (first index is 1)
+
+	// volatile state on all servers
+	state              CMState   // state of this CM (Follower, Candidate, Leader, Dead, Shutdown)
+	electionResetEvent time.Time // used to reset election timer
+}
+
+// Server is the RPC end point that receives RPCs from peers.
+type Server struct {
+	mu       sync.Mutex
+	serverId int
+	peerIds  []int
+	cm       *ConsensusModule
+
+	rpcProxy  *RPCProxy     // proxy to send RPCs to peers
+	rpcServer *rpc.Server   // RPC server to receive RPCs from peers
+	listener  *net.Listener // listener to accept RPC connections from peers
+
+	peerClients map[int]*rpc.Client // RPC clients to send RPCs to peers
+
+	wg    sync.WaitGroup
+	ready <-chan interface{}
+	quit  chan interface{}
+}
+
+// LogEntry is a single entry in the log.
+type LogEntry struct {
+	Command interface{}
+	Term    int
+}
+
+type RPCProxy struct {
+	cm *ConsensusModule
+}
+
+// RequestVoteArgs is the arguments for the RequestVote RPC.
+type RequestVoteArgs struct {
+	Term         int
+	CandidateId  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+// RequestVoteReply is the reply for the RequestVote RPC.
+type RequestVoteReply struct {
+	Term        int
+	VoteGranted bool
+}
+
+// AppendEntriesArgs is the arguments for the AppendEntries RPC.
+type AppendEntriesArgs struct {
+	Term         int
+	LeaderId     int
+	PrevLogIndex int
+	PrevLogTerm  int
+	LeaderCommit int
+	Entries      []LogEntry
+}
+
+// AppendEntriesReply is the reply for the AppendEntries RPC.
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
